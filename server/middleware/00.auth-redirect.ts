0602e20b295af28a4c3b47601f86fa12ac60a2d6
i4nizer <iansandoval264@gmail.com>
2025-07-11 12:31:29 +0800

    Changed to check both access and refresh token before redirecting.

--- server/middleware/00.auth-redirect.ts
+++ server/middleware/00.auth-redirect.ts
@@ -3,12 +3,14 @@
 export default defineEventHandler(async (event) => {
     // --- Token Check
     const accessToken = getCookie(event, "access-token")
+    const refreshToken = getCookie(event, "refresh-token")
+    const authorizable = !!accessToken || !!refreshToken
     
     // --- Route Control
     const url = getRequestURL(event)
     const isProtected = url.pathname.startsWith("/user") || url.pathname.startsWith("/api/user")
     const isAuthorization = url.pathname.startsWith("/auth")
 
-    if (isProtected && !accessToken) return await sendRedirect(event, "/auth/sign-in")
-    if (isAuthorization && accessToken) return await sendRedirect(event, "/user/greenhouse")
+    if (isProtected && !authorizable) return await sendRedirect(event, "/auth/sign-in")
+    if (isAuthorization && authorizable) return await sendRedirect(event, "/user/greenhouse")
 })
