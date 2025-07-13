1ff3a303c8b766479ea2992d514e6e77bf4acf05
i4nizer <iansandoval264@gmail.com>
2025-07-13 08:09:41 +0800

    Directly accessed 'event.context.accessTokenPayload' to avoid redundancy check.

--- server/api/user/index.get.ts
+++ server/api/user/index.get.ts
@@ -11,12 +11,7 @@ type AuthUser = z.infer<typeof AuthUserSchema>
 
 export default defineEventHandler(async (event) => {
     // --- Use Token as Identity
-    const accessToken = getCookie(event, "access-token")
-    if (!accessToken) return sendError(event, createError({ statusCode: 401, statusMessage: "Authorization required." }))
-    
-    const accessTokenResult = safeVerifyToken<{ id: number, name: string, email: string }>(accessToken, "Access")
-    if (!accessTokenResult.success) return sendError(event, createError({ statusCode: 401, statusMessage: "Access invalidated." }))
-    const payload = accessTokenResult.data
+    const payload = event.context.accessTokenPayload as { id: number; name: string; email: string }
     
     // --- Find user from payload
     const user = await User.findOne({ where: { id: payload.id } })
